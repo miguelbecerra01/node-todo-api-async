679054480090b4b420dc320f92ec2c43f4cd095d
Miguel BEcerra <miguelbecerra01@gmail.com>
2019-02-21 13:07:53 -0300

    Add connection strings for mongodb external

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 //let mongoose use promises
 mongoose.Promise = global.Promise;
 
-const externalUrl = 'mongodb+srv://miguel:miguelb1!@mongodbtestapps-ykl1d.mongodb.net/test?retryWrites=true';
+const externalUrl = 'mongodb://miguel:miguelb1!@mongodbtestapps-shard-00-00-ykl1d.mongodb.net:27017,mongodbtestapps-shard-00-01-ykl1d.mongodb.net:27017,mongodbtestapps-shard-00-02-ykl1d.mongodb.net:27017/test?ssl=true&replicaSet=mongodbTestApps-shard-0&authSource=admin&retryWrites=true';
 const localUrl = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(externalUrl, { useNewUrlParser: true });
