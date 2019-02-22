77f1b0c95de9650ec8911f4869e0c38fc1f7fab8
Miguel BEcerra <miguelbecerra01@gmail.com>
2019-02-22 17:25:56 -0300

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -345,4 +345,30 @@ describe('POST /todos', () => {
 
     });
 
+    describe('DELETE /users/me/token', () => {
+        it('should remove auth token on logout', (done) => {
+            request(app)
+                .delete('/users/me/token')
+                .set('x-auth', users[0].tokens[0].token)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toEqual({});
+                })
+                .end((err, res) => {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    User.findById(users[0]._id).then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch((e) => {
+                        done(e);
+                    });
+                });
+
+        });
+    });
+
+
 });
